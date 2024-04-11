94d4d636bab0c558b0e628e9e8bc7fa413d40ab3
sourcebunny <sourcebunny@posteo.com>
2024-04-10 21:35:55 -0700

    move llamafile into docker background

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 # Run llamafile if model.gguf is present
 if [ -f "/data/model.gguf" ]; then
   echo "Model file found. Running llamafile..."
-  /app/llamafile -m /data/model.gguf -ngl 0
+  /app/llamafile -m /data/model.gguf -ngl 0 &
 else
   echo "No model file found. Skipping llamafile execution..."
 fi
